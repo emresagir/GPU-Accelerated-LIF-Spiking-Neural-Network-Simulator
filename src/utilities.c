312a1b49ced657d97ee5177f50d992f478c4394e
gpu_group9 gpu_group9 <gpu_group9@login1.cluster.lan>
2026-01-11 14:36:43 +0000

    added: small fix

--- src/utilities.c
+++ src/utilities.c
@@ -5,6 +5,9 @@
 
 void loadw_f32(const char *fn, size_t count, float arr[]) {
     FILE *f = fopen(fn,"rb");
+    if (!f) {
+        printf("File couldn't opened \n");
+    }
     float *buf = malloc(sizeof(float) * count);
     size_t r = fread(buf, sizeof(float), count, f);
     if(r != count) fprintf(stderr,"read %zu of %zu elements\n", r, count);
@@ -16,6 +19,9 @@ void loadw_f32(const char *fn, size_t count, float arr[]) {
 void load_ext_spikes(const char *fn, uint8_t arr[]){
     uint8_t *buf = malloc(T * N);
     FILE *f = fopen(fn,"rb");
+    if (!f) {
+        printf("File couldn't opened \n");
+    }
     fread(buf, 1, T * N, f);
     fclose(f);
     memcpy(arr, buf, sizeof(uint8_t) * T * N);
